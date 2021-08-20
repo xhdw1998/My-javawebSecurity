760b9a08de0e86cd993f75a7f61869bed2cf3dba
xiaohua <1098395580@qq.com>
2021-08-20 11:03:49 +0800

    “xiaohua2021”

---
+++ Demo.java
@@ -0,0 +1,9 @@
+package com.xiaohua;
+public class Demo(){
+	public static void main(String[] args){
+		System.out.println("hello world");
+	}
+	// public Demo(){
+	// 	System.out.println("demo instance");
+	// }
+}
